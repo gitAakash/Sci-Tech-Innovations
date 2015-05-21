4692e0639cddcda402d605b5b152034ab3831619
aakashp <aakash.pawar@wwindia.com>
2015-05-21 18:46:49 +0530

    Code base

---
+++ Sci-TechInnovation.Web.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sci_TechInnovation.Web.UI.MvcApplication" Language="C#" %>
---
+++ Sci-TechInnovation.Web.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sci_TechInnovation.Web.UI.Startup))]
+namespace Sci_TechInnovation.Web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
